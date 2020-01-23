3ccc581067a83500f4046d738376c52e3aa811b8
Duncan Robbins and Patrick Delaney <patrick@>
2020-01-23 10:04:47 -0800

    allow program to recognize letters regardless of capitalization

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
 $(document).ready(function(){
     let vowels = ["a", "e", "i", "o", "u", "y"]
     let consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
-    let alph = vowels.concat(consonants)
+    let letters = vowels.concat(consonants)
+    let lettersUpper = letters.toString().toUpperCase().split("")
     $("#user-Form").submit(function(event){
         event.preventDefault();
         var userInputs = $("input#user-text").val().split("")
         console.log(userInputs);
         userInputs.forEach(function(userInput){
             
-            if(alph.includes(userInput)){
+            if(letters.join(lettersUpper).includes(userInput)){
                 console.log(userInput);
                 
             }
             else{
-                console.log("please enter a number");
+                console.log("please do not enter a number");
                 
             }
         });
