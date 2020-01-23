7d72d84db6436e4c61121580f218cbc94251f00a
Duncan Robbins and Patrick Delaney <patrick@>
2020-01-23 10:26:06 -0800

    allow program to ad ay to single vowels

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,22 @@ $(document).ready(function(){
     let vowels = ["a", "e", "i", "o", "u", "y", "A", "E", "I", "O", "U", "Y"]
     let consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"]
     let letters = vowels.concat(consonants)
-    let lettersUpper = letters.toString().toUpperCase().split("")
+    
     $("#user-Form").submit(function(event){
+        let result = []
         event.preventDefault();
         var userInputs = $("input#user-text").val().split("")
         console.log(userInputs);
         userInputs.forEach(function(userInput){
             
-            if(letters.join(lettersUpper).includes(userInput)){
+            if(letters.includes(userInput)){
                 console.log(userInput);
                 
                 if  ((userInputs.length === 1) && (vowels.includes(userInputs[0]))) {
+                    result.push(userInputs + "ay")
 
-                    console.log("is single vowel");
                     
+                
                 }
                 else {
                     console.log("not single vowel");
@@ -35,9 +37,11 @@ $(document).ready(function(){
 
             }
             else{
-                console.log("please do not enter a number");
+                result.push("please do not enter a number");
                 
             }
         });
+        console.log(result);
+        
     })
 })
